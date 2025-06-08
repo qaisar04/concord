b1526f775b0b1af621db07588a22076fe7b07293
Балтабаев Кайсар Бегисович <kaisar.baltabayev@kaspi.kz>
2025-06-08 15:59:49 +0500

    feat: refactor mongo configuration classes to use constructor injection

--- concord-mongo-autoconfigure/src/main/java/kz/concord/concord_mongo_autoconfigure/config/ConcordMongoAutoConfiguration.java
+++ concord-mongo-autoconfigure/src/main/java/kz/concord/concord_mongo_autoconfigure/config/ConcordMongoAutoConfiguration.java
@@ -3,17 +3,21 @@ package kz.concord.concord_mongo_autoconfigure.config;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import kz.concord.concord_mongo_autoconfigure.config.props.ConcordMongoProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableConfigurationProperties(ConcordMongoProperties.class)
 public class ConcordMongoAutoConfiguration {
 
+    private final ConcordMongoProperties properties;
+
     @Bean
-    public MongoClient mongoClient(ConcordMongoProperties properties) {
+    public MongoClient mongoClient() {
         StringBuilder uriBuilder = new StringBuilder("mongodb://");
 
         if (properties.getCredentials().getUsername() != null && properties.getCredentials().getPassword() != null) {
@@ -29,7 +33,7 @@ public class ConcordMongoAutoConfiguration {
     }
 
     @Bean
-    public ReactiveMongoTemplate reactiveMongoTemplate(MongoClient mongoClient, ConcordMongoProperties properties) {
+    public ReactiveMongoTemplate reactiveMongoTemplate(MongoClient mongoClient) {
         return new ReactiveMongoTemplate(mongoClient, properties.getDatabase());
     }
 
--- concord-mongo-autoconfigure/src/main/java/kz/concord/concord_mongo_autoconfigure/config/MongoMonitoringConfiguration.java
+++ concord-mongo-autoconfigure/src/main/java/kz/concord/concord_mongo_autoconfigure/config/MongoMonitoringConfiguration.java
@@ -2,6 +2,7 @@ package kz.concord.concord_mongo_autoconfigure.config;
 
 import kz.concord.concord_mongo_autoconfigure.config.props.ConcordMongoProperties;
 import kz.concord.concord_mongo_autoconfigure.monitoring.MongoLoggingCommandListener;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,12 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableConfigurationProperties(ConcordMongoProperties.class)
 public class MongoMonitoringConfiguration {
 
+    private final ConcordMongoProperties properties;
+
     @Bean
     @ConditionalOnProperty(prefix = "concord.mongo.monitoring", name = "enabled", havingValue = "true")
-    public MongoClientSettingsBuilderCustomizer monitoringCustomizer(ConcordMongoProperties properties) {
+    public MongoClientSettingsBuilderCustomizer monitoringCustomizer() {
         return clientSettingsBuilder -> clientSettingsBuilder
                 .addCommandListener(new MongoLoggingCommandListener(properties.getMonitoring().getLogLevel()));
     }
